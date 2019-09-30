d26f3265f556b7a4f1a79e2a2fca00761154a144
Daniel <d.tuytuyshkin@gmail.com>
2019-09-30 22:22:00 +0300

    testing working with date

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <functional>
 #include <algorithm>
+#include <ctime>
 
 using std::string;
 using std::cin;
@@ -504,20 +505,85 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
 }
 
 //-------------------------
+//-------------------------
+//-------------------------
+
+// PART 2
+
+//-------------------------
+//-------------------------
+//-------------------------
+
+
+class Date {
+    public:
+        int year;
+        int month;
+        int day;
+        int hours;
+        int minutes;
+        int seconds;
+    };
+
+
+
+bool operator<(const Date& date1, const Date& date2) { return true; }
+bool operator==(const Date& date1, const Date& date2) { return (date1 == date2); }
+
+// Make a tm structure representing this date
+std::tm make_tm(int year, int month, int day)
+{
+    std::tm tm = {0};
+    tm.tm_year = year - 1900; // years count from 1900
+    tm.tm_mon = month - 1;    // months count from January=0
+    tm.tm_mday = day;         // days count from 1
+    return tm;
+}
+
+//-------------------------
 int main() {
-    using std::vector;
 
-    cout << endl << "Working with Variable Tree:" << endl;
-    Tree<vector<int>>* tree1 = new VariableSonTree<vector<int>>({1,2});
-    test_tree(tree1);
 
-    cout << endl << "Working with Binary Tree:" << endl;
-    BinTree<double>* tree2 = new BinTree<double>(0.1);
-    test_tree(tree2);
 
-    cout << endl << "Working with Search Tree:" << endl;
-    Tree<int>* tree3 = new SearchTree<int>{1};
-    test_tree(tree3);
+// Structures representing the two dates
+    std::tm tm1 = make_tm(2012,4,2);    // April 2nd, 2012
+    std::tm tm2 = make_tm(2003,2,2);    // February 2nd, 2003
+
+// Arithmetic time values.
+// On a posix system, these are seconds since 1970-01-01 00:00:00 UTC
+    std::time_t time1 = std::mktime(&tm1);
+    std::time_t time2 = std::mktime(&tm2);
+
+// Divide by the number of seconds in a day
+    const int seconds_per_day = 60*60*24;
+    std::time_t difference = (time1 - time2);
+
+// To be fully portable, we shouldn't assume that these are Unix time;
+// instead, we should use "difftime" to give the difference in seconds:
+    double portable_difference = std::difftime(time1, time2) / seconds_per_day;
+
+    cout << portable_difference;
+    cout << difference;
+    struct tm *tminfo;
+
+    tminfo = localtime ( &difference );
+    printf ( "Current date and time are: %s", asctime (tminfo) );
+
+
+//
+//    using std::vector;
+//
+//    cout << endl << "Working with Variable Tree:" << endl;
+//    Tree<vector<int>>* tree1 = new VariableSonTree<vector<int>>({1,2});
+//    test_tree(tree1);
+//
+//    cout << endl << "Working with Binary Tree:" << endl;
+//    BinTree<double>* tree2 = new BinTree<double>(0.1);
+//    test_tree(tree2);
+//
+//    cout << endl << "Working with Search Tree:" << endl;
+//    Tree<int>* tree3 = new SearchTree<int>{1};
+//    test_tree(tree3);
     return 0;
 }
 
