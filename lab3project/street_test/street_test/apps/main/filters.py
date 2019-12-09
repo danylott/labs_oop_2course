2d5027752c2edfb4ea460a8b26e58e89c2245f0b
Daniel <d.tuytuyshkin@gmail.com>
2019-12-09 20:28:53 +0200

    some little changes, to finish my this year work

--- lab3project/street_test/street_test/apps/main/filters.py
+++
@@ -1,9 +0,0 @@
-from .models import Street
-import django_filters
-
-class StreetFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='icontains')
-
-    class Meta:
-        model = Street
-        fields = ['name', 'type']
--- lab3project/street_test/street_test/apps/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'main'
-urlpatterns = [
-    path('', views.search, name='search'),
-    path('<int:street_id>/', views.detail, name='detail'),
-]
