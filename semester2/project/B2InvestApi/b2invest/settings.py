736099d5e977062c9aab48fb7699edb60f52585d
Daniel <d.tuytuyshkin@gmail.com>
2020-04-18 09:08:45 +0300

    add django cors headers for sending data to react

--- semester2/project/B2InvestApi/b2invest/settings.py
+++ semester2/project/B2InvestApi/b2invest/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'django_filters',
     'phonenumber_field',
     'drf_yasg',
@@ -48,12 +49,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
 
 ROOT_URLCONF = 'b2invest.urls'
 
